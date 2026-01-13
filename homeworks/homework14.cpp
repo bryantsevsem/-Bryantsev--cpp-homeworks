3ee541ad88fcbd115134a308a76da7ff434ed7c9
bryantsevsem <semikbryantsev@gmail.com>
2026-01-13 14:27:53 +0500

    Update homework14.cpp

--- homeworks/homework14.cpp
+++ homeworks/homework14.cpp
@@ -1,72 +1,28 @@
 #include <iostream>
-#include <cctype>
-#include <string>
 #include <vector>
+#include <string>
 using namespace std;
 
-static string cleanToken(string s) {
-    size_t left = 0;
-    while (left < s.size() && !(isalnum(static_cast<unsigned char>(s[left])) || s[left] == '_' || s[left] == '-')) {
-        left++;
-    }
-    size_t right = s.size();
-    while (right > left && !(isalnum(static_cast<unsigned char>(s[right - 1])) || s[right - 1] == '_' || s[right - 1] == '-')) {
-        right--;
-    }
-    return s.substr(left, right - left);
-}
-
-static bool isIntegerToken(const string &s) {
-    if (s.empty()) {
-        return false;
-    }
-    size_t i = 0;
-    if (s[0] == '-') {
-        if (s.size() == 1) {
-            return false;
-        }
-        i = 1;
-    }
-    for (; i < s.size(); i++) {
-        if (!(s[i] >= '0' && s[i] <= '9')) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
-    vector<string> tokens;
-    string token;
-    while (cin >> token) {
-        token = cleanToken(token);
-        if (!token.empty()) {
-            tokens.push_back(token);
-        }
+
+    int n;
+    cin >> n;
+
+    vector<string> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    vector<bool> res;
+    res.reserve(n);
+
+    bool state = true;
+    for (const string& s : a) {
+        if (s == "flick") state = !state;   // переключаем начиная с этого элемента
+        res.push_back(state);
     }
 
-    int startIndex = 0;
-    int n = static_cast<int>(tokens.size());
-    if (!tokens.empty() && isIntegerToken(tokens[0])) {
-        n = stoi(tokens[0]);
-        startIndex = 1;
-        if (startIndex + n > static_cast<int>(tokens.size())) {
-            n = static_cast<int>(tokens.size()) - startIndex;
-        }
+    for (int i = 0; i < n; ++i) {
+        cout << (res[i] ? "1" : "0");
+        if (i + 1 < n) cout << ' ';
     }
-
-    bool current = true;
-    cout << '[';
-    for (int i = 0; i < n; i++) {
-        const string &word = tokens[startIndex + i];
-        if (word == "flick") {
-            current = !current;
-        }
-        cout << boolalpha << current;
-        if (i + 1 < n) {
-            cout << ", ";
-        }
-    }
-    cout << ']';
     return 0;
 }
